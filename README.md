c10960a5fe0950af10853e6957c89492b414eb73
Rich Grundy <rich@27smiles.com>
2013-04-09 09:55:59 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# chef-capybara-webkit cookbook
+
+Install [capybara-webkit](https://github.com/thoughtbot/capybara-webkit) dependencies.
+
+# Author
+
+Author:: Rich Grundy (rich@27smiles.com)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "chef-capybara-webkit"
+maintainer       "Richard Grundy"
+maintainer_email "rich@27smiles.com"
+license          "All rights reserved"
+description      "Installs capybara-webkit dependencies"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
